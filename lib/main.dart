73de7b2b48317235e152e830f4da0703a2be227f
Eunho Lee <eunho.rhee@gmail.com>
2020-01-28 18:56:40 +0900

    whitelogin

--- lib/main.dart
+++ lib/main.dart
@@ -1,29 +1,22 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bundang/screens/login_screen.dart';
+import 'package:flutter_bundang/screens/login/login_screen.dart';
 
 void main() => runApp(MyApp());
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  const MyApp({Key key}) : super(key: key);
 
-  Widget _getScreenId() {
-    return LoginScreen();
+  @override
+  _MyAppState createState() => _MyAppState();
+}
 
-  }
-
+class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Travel Monday',
-        debugShowCheckedModeBanner: false,
-        theme: ThemeData(
-          primaryColor: Color(0xFF3EBACE),
-          accentColor: Color(0xFFD8ECF1),
-          scaffoldBackgroundColor: Color(0xFFF3F5F7),
-        ),
-        home: _getScreenId(),
-        routes: {
-          //LoginScreen.id: (context) => LoginScreen(),
-        }
+      title: "Bank App Neomorphism",
+      debugShowCheckedModeBanner: false,
+      home: LoginScreen(),
     );
   }
 }
